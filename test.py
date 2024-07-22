d61e8d9159495a59f1dc201163bf2fe28d4fc123
Barry <tun32913@temple.edu>
2024-07-22 19:45:30 -0400

    testing
    test commit to branch

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from kivy.app import App
 from kivy.uix.label import Label 
 class BasicApp(App):
     def build(self):
-        return Label(text="Hello World!")
+        return Label(text="Hello World! This is testing the branch")
 if __name__ == "__main__":
     BasicApp().run()
+    
     
