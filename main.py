1be52ba2d32f927893bc6d064912f07c77510e62
AhmetYellow <145231474+AhmetYellow@users.noreply.github.com>
2024-07-30 00:44:47 -0400

    Made a working button that takes in each field, and resets them

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import kivy
 from kivy.app import App
+from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
-class MyGrid():
-    pass
+class MyGrid(Widget):
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+    re_type_password = ObjectProperty(None)
+
+    def btn(self):
+        print("Name: ", self.name.text, "Email: ", self.email.text, "Password: ", self.password.text, "Re-type Password: ", self.re_type_password.text)
+        self.name.text = ""
+        self.email.text = ""
+        self.password.text = ""
+        self.re_type_password.text = ""
+
 
 class MyApp(App):
     def build(self):
         return MyGrid()
     
-
 if __name__ == "__main__":
     MyApp().run()
