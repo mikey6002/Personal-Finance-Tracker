9bba352f6a9302d5948fa58e65d444b74ba95d8b
Barry <tun32913@temple.edu>
2024-07-30 13:44:45 -0400

    Changed Navbar to match

---
+++ main.py
@@ -0,0 +1,18 @@
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+from kivymd.app import MDApp
+from kivymd.uix.scrollview import MDScrollView
+
+class ContentNavigationDrawer(MDScrollView):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+class Example(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette = "Purple"
+        self.theme_cls.theme_style = "Light"
+        return Builder.load_file('my.kv')
+
+if __name__ == '__main__':
+    Example().run()
