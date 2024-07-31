bf9e525d2ee0137e9c70c9c57ed9418ea4dd9b58
AhmetYellow <145231474+AhmetYellow@users.noreply.github.com>
2024-07-31 19:39:58 -0400

    kivymd finally works, this is my initial code

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivymd.app import MDApp 
 
-class MyGrid(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
-    password = ObjectProperty(None)
-    re_type_password = ObjectProperty(None)
-
-    def btn(self):
-        print("Name: ", self.name.text, "Email: ", self.email.text, "Password: ", self.password.text, "Re-type Password: ", self.re_type_password.text)
-        self.name.text = ""
-        self.email.text = ""
-        self.password.text = ""
-        self.re_type_password.text = ""
-
-
-class MyApp(App):
+class MainApp(MDApp):
     def build(self):
-        return MyGrid()
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "BlueGray"
+        return Builder.load_file('login.kv')
     
-if __name__ == "__main__":
-    MyApp().run()
+MainApp().run()
