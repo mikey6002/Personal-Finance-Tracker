b9e10494f4fae50d432bfdab30b8a9902c920ae7
AhmetYellow <145231474+AhmetYellow@users.noreply.github.com>
2024-08-01 01:46:04 -0400

    Fields ready, icons ready, pswrd checkbox working

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp 
+from kivymd.uix.selectioncontrol import MDCheckbox
 
 class MainApp(MDApp):
+
+
     def build(self):
-        self.theme_cls.theme_style = "Light"
-        self.theme_cls.primary_palette = "BlueGray"
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "LightBlue"
         return Builder.load_file('login.kv')
-    
+    def signer(self):
+        self.root.ids.welcome_label.text = f'Welcome in, {self.root.ids.username.text}'
+        print("Username: ", self.root.ids.username.text, "Email: ", self.root.ids.email.text, "Password: ", self.root.ids.password.text, "Re-typed Password: ", self.root.ids.repassword.text)
+        self.root.ids.username.text = ""
+        self.root.ids.email.text = ""
+        self.root.ids.password.text = ''
+        self.root.ids.repassword.text = ''
+
 MainApp().run()
